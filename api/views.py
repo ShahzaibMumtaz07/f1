7cc0cc4b26407bcd7cf077aee2665ac0a00faa47
ShahzaibMumtaz07 <shahzaib.mumtaz.20195@gmail.com>
2023-02-22 15:37:01 +0500

    API:changes in views and added templates

--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.conf import settings
-
+from api.models import Circuits
 
 def indexpage(request):
     # locations = [{i:j for i,j in k.items() if i in ['lat','lng','name']} for k in df.to_dict('records')]
-    # locations = [dict(item, **{'size':1000 * 4e-4,'color':'white'}) for item in locations]
-    context = {'data22':None}
+    locations = Circuits.objects.all().values('lat','lng','name')
+    locations = [dict(item, **{'size':1000 * 4e-4,'color':'white'}) for item in locations]
+    context = {'data22':locations}
     
-    return render(request, 'index2.html', context)
+    return render(request, 'index.html', context)
