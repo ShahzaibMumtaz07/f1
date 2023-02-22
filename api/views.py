6ff988d123bb2d5f755b52cf9f4a805b83413fc9
ShahzaibMumtaz07 <shahzaib.mumtaz.20195@gmail.com>
2023-02-22 21:19:06 +0500

    API: worked on the css

--- api/views.py
+++ api/views.py
@@ -3,9 +3,8 @@ from django.conf import settings
 from api.models import Circuits
 
 def indexpage(request):
-    # locations = [{i:j for i,j in k.items() if i in ['lat','lng','name']} for k in df.to_dict('records')]
-    locations = Circuits.objects.all().values('lat','lng','name')
-    locations = [dict(item, **{'size':1000 * 4e-4,'color':'white'}) for item in locations]
-    context = {'data22':locations}
+    circuits = Circuits.objects.all().values('lat','lng','name')
+    circuits = [dict(item, **{'size':1000 * 4e-4,'color':'white'}) for item in circuits]
+    context = {'circuits':circuits}
     
     return render(request, 'index.html', context)
