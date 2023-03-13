b0f414f07aecc470f89c47c7dec9bdf802de7e75
ShahzaibMumtaz07 <shahzaib.mumtaz.20195@gmail.com>
2023-03-13 06:36:22 +0500

    API: added django_debug_toolbar for development

--- f1/settings/development.py
+++ f1/settings/development.py
@@ -1,25 +1,31 @@
 from .base import *
 import sys
+import socket
+
 
 DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE.insert(3,"debug_toolbar.middleware.DebugToolbarMiddleware")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "test_db",
-        "USER": "postgres",
-        "PASSWORD": "test@123",
-        "HOST": "localhost",
-        "PORT": "5432",
-    },
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
+    }
 }
 if 'test' in sys.argv:
     DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "test_db",
+        "NAME": "test_postgres",
         "USER": "postgres",
-        "PASSWORD": "test@123",
+        "PASSWORD": "password",
         "HOST": "localhost",
         "PORT": "5432",
     },
@@ -27,11 +33,12 @@ if 'test' in sys.argv:
 
 # TEST_RUNNER = 'weather_app.tests.utils.UnManagedModelTestRunner'
 
-
 CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
-        "LOCATION": "my_cache",
-        "KEY_PREFIX": "dev",
-    }
+        'default': {
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+            'LOCATION': 'unique-snowflake',
+        }
 }
+
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
